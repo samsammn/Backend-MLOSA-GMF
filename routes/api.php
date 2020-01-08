cdd1bb23c43c8df6806c6695a9ddbb968764ba11
samsam.nursamsi02 <samsam.nursamsi02@gmail.com>
2020-01-09 06:37:39 +0700

    add middleware cors

--- routes/api.php
+++ routes/api.php
@@ -15,22 +15,26 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Resources\ResultCollection;
 use Illuminate\Support\Facades\DB;
 
-Route::post('/signin', 'UserController@signin');
-Route::resource('user', 'UserController');
+Route::group(['middleware' => 'cors'], function () {
 
-Route::resource('observation', 'ObservationController');
+    Route::post('/signin', 'UserController@signin');
+    Route::resource('user', 'UserController');
 
-Route::get('/chart_safety', function(){
-    $data = DB::table('maintenance_processes')
-                ->select(DB::raw('maintenance_process_details.id, maintenance_processes.name as maintenance_name, activities.name as activity_name, sub_activities.description'))
-                ->join('maintenance_process_details', 'maintenance_process_details.mp_id', 'maintenance_processes.id')
-                ->join('activities', 'activities.id','maintenance_process_details.activity_id')
-                ->join('sub_activities', 'sub_activities.id', 'maintenance_process_details.sub_activity_id')
-                ->get();
-    return $data;
-});
+    Route::resource('observation', 'ObservationController');
 
-Route::get('/chart_test', function() {
-    $data = DB::select('select * from vw_maintenance_process');
-    return $data;
+    Route::get('/chart_safety', function(){
+        $data = DB::table('maintenance_processes')
+                    ->select(DB::raw('maintenance_process_details.id, maintenance_processes.name as maintenance_name, activities.name as activity_name, sub_activities.description'))
+                    ->join('maintenance_process_details', 'maintenance_process_details.mp_id', 'maintenance_processes.id')
+                    ->join('activities', 'activities.id','maintenance_process_details.activity_id')
+                    ->join('sub_activities', 'sub_activities.id', 'maintenance_process_details.sub_activity_id')
+                    ->get();
+        return $data;
+    });
+
+    Route::get('/chart_test', function() {
+        $data = DB::select('select * from vw_maintenance_process');
+        return $data;
+    });
+
 });
